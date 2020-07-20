0e858335d2a51a73bcebe89c0aa71b75ee050ac2
jsotoca <jsotoca@unprg.edu.pe>
2020-07-19 22:05:14 -0500

    pagina contenido y modulo para componentes creados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'contenido',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'contenido',
+    loadChildren: () => import('./paginas/contenido/contenido.module').then( m => m.ContenidoPageModule)
   },
 ];
 
--- src/app/paginas/contenido/contenido.page.spec.ts
+++ src/app/paginas/contenido/contenido.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { ContenidoPage } from './contenido.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('ContenidoPage', () => {
+  let component: ContenidoPage;
+  let fixture: ComponentFixture<ContenidoPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ ContenidoPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(ContenidoPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
